47210744e377af98367e4a17a1a1cdb04f8179c6
peyday49 <pbrantley49@gmail.com>
2024-03-12 21:47:57 -0500

    Initial Card Class

---
+++ Card.js
@@ -0,0 +1,11 @@
+/* Card Class for Clue-Less*/
+
+//Initialization
+class Card {
+    constructor(type, name, imageURL) {
+        this.type = type;
+        this.name = name;
+        this.imageURL = imageURL;
+    }
+}
+
