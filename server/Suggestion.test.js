b9128fab19039fdffd9bb33e83a56ad52d58f347
peyday49 <99624148+peyday49@users.noreply.github.com>
2024-03-24 15:56:21 -0500

    Update Suggestion.test.js

--- server/Suggestion.test.js
+++ server/Suggestion.test.js
@@ -1,4 +1,4 @@
-const makeSuggestion = require("../Driver");
+/*const makeSuggestion = require("../Driver");
 const assert = require("assert");
 describe("Make Suggestion", function(){
     describe ("suggestion" , function() {
@@ -8,3 +8,27 @@ describe("Make Suggestion", function(){
         })
         })
     });
+    */
+
+    const {makeSuggestion} = require("../driver");
+    const assert = require("assert");
+    describe("Make Suggestion", function() {
+        describe("suggestion" , function() {
+            it("should create a valid suggestion object" , function() {
+                const player = {room: "Kitchen"};
+                const suspect = "Jim";
+                const weapon = "Coffee Mug";
+                const accusation = false;
+                
+                const expectedSuggestion = {
+                    suspect: "Jim", 
+                    weapon: "Coffee Mug", 
+                    room: "Kitchen", 
+                };
+
+                const suggestion = makeSuggestion(player, suspect, weapon, room, accusation);
+                assert.deepStrictEqual(suggestion, expectedSuggestion);
+            });
+        });
+    });
+    //
